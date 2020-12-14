696155fc59808fb689428c9fdc3321cdc713ba5d
Guilherme469 <guilhermeluiz06@hotmail.com>
2020-12-14 18:11:16 -0300

    javascript

---
+++ Study/JavaScript/js/main.js
@@ -0,0 +1,83 @@
+/*
+var nome = "Guilherme Luiz";
+var idade = 18
+alert(nome + " tem " + idade + " anos de idade")
+*/
+
+/*var frase = "Japao e o melhor time";
+console.log(frase);
+console.log(frase.toUpperCase());
+*/
+
+/*
+var lista = ["maca","pera","limao","mandioca"];
+lista.push("uva");
+console.log(lista)
+console.log(lista.toString());
+*/
+
+/*
+var fruta = {nome: "maca", cor: "vermelha"};
+console.log(fruta.nome);
+*/
+
+/*
+var idade = prompt("Qual sua idade? ");
+if(idade >= 18){
+    alert("Maior de idade")
+}else{
+    alert("Menor de idade")
+}
+*/
+
+/*
+var count = 0
+while(count <= 5){
+    alert(count);
+    count++
+}
+*/
+
+/*
+var data = new Date;
+alert(data.getMinutes();
+*/
+
+/*
+function soma(n1,n2) {
+    return n1 + n2;
+}
+
+alert(soma(5,50));
+*/
+
+/*
+function validaIdade(idade){
+var validar;
+if(idade >= 18){
+    validar = true;
+}else{
+    validar = false;
+}
+    return validar;
+}
+
+var idade = prompt("Qual sua idade");
+console.log(validaIdade(idade));
+*/
+function outra(elemento){
+    console.log(elemento.value);
+}
+
+
+function botao(elemento){
+    elemento.innerHTML = "Obrigado por clicar"
+}
+
+function fora(elemento){
+    elemento.innerHTML = "Clique aqui"
+}
+
+function redirecionar(){
+    window.open("https://www.youtube.com");
+}
